db08b487e3d5ffee8c371b8ea511536a3332a15b
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-11-26 23:13:31 +0530

    adjust ports and create a top level README file

--- services/inference/main.py
+++ services/inference/main.py
@@ -3,5 +3,5 @@ from src import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("src:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("src:app", host="127.0.0.1", port=8002, reload=True)
 
--- services/media_store/main.py
+++ services/media_store/main.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("src:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src:app", host="127.0.0.1", port=8001, reload=True)
 
