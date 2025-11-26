52fa0c04405cd7034587b5835e1082561cc8ee12
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-11-26 23:04:52 +0530

    path issue, run in local folder

--- services/inference/main.py
+++ services/inference/main.py
@@ -1,7 +1,7 @@
-from services.inference.src import app
+from src import app
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("services.inference.src:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("src:app", host="127.0.0.1", port=8001, reload=True)
 
--- services/media_store/main.py
+++ services/media_store/main.py
@@ -1,7 +1,5 @@
-from services.media_store.src import app
-
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src:app", host="127.0.0.1", port=8000, reload=True)
 
