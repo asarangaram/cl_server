a14e88d2112eee3cc4d266c29d41e1f2e6b2d749
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-11-26 21:00:32 +0530

    delete few unwanted files.

--- vector_store/setup_path.sh
+++
@@ -1,3 +0,0 @@
-# Add qdrant_docker/bin to PATH for easy access to vector_store_start/vector_store_stop scripts
-export PATH="$PATH:/Users/anandasarangaram/Work/github/cl_server/vector_store/qdrant_docker/bin"
-
